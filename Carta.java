38dd11b63b9d2449272d08b5c615e4a8cf76b087
Benja Bavera <benjabavera@gmail.com>
2024-06-27 20:18:07 -0300

    a

---
+++ Carta.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Cartas here.
+ * 
+ * @author KulilinWP
+ * @version 0.1
+ */
+public class Carta
+{
+   private String nombre;
+   private int poder;
+   
+   public Carta(String nombre, int poder){
+       if ()
+       this.nombre = nombre;
+       this.poder = poder;
+    }
+}
--- Jugador.java
+++ Jugador.java
@@ -5,7 +5,7 @@
  * @author KulilinWP
  * @version 0.1
  */
-public class Jugadores
+public class Jugador
 {
    
 }
