d2a30badf212f8a9f78e68f0eab15cdd930de96e
Benjamin Bavera <163863090+KulilinWP@users.noreply.github.com>
2024-06-27 21:24:38 -0300

    Update Mazo.java

--- Mazo.java
+++ Mazo.java
@@ -8,19 +8,19 @@ import java.util.*;
  */
 public class Mazo
 {
-   private Carta[] cartas;
+   private ArrayList<Carta> cartas;
 
    public Mazo(){
-      cartas = new Carta[10];
-      cartas[0] = new Carta("CR7",99);
-      cartas[1] = new Carta("Messi",99);
-      cartas[2] = new Carta("Neymar",95);
-      cartas[3] = new Carta("Kroos",90);
-      cartas[4] = new Carta("Iniesta",94);
-      cartas[5] = new Carta("Ramos",91);
-      cartas[6] = new Carta("De Bruyne",90);
-      cartas[7] = new Carta("Merentiel",75);
-      cartas[8] = new Carta("Courtois",88);
-      cartas[9] = new Carta("Modric",90);
+      cartas = new ArrayList();
+      cartas.add(new Carta("CR7",99));
+      cartas.add(new Carta("Messi",99));
+      cartas.add(new Carta("Neymar",95));
+      cartas.add(new Carta("Kroos",90));
+      cartas.add(new Carta("Iniesta",94));
+      cartas.add(new Carta("Ramos",91));
+      cartas.add(new Carta("De Bruyne",90));
+      cartas.add(new Carta("Merentiel",75));
+      cartas.add(new Carta("Courtois",88));
+      cartas.add(new Carta("Modric",90));
    }
 }
