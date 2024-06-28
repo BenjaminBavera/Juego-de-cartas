9e9987e59cbc9421f792ea7e6cff2a3740cbb84b
Benjamin Bavera <163863090+KulilinWP@users.noreply.github.com>
2024-06-27 21:13:42 -0300

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -7,5 +7,12 @@
  */
 public class Jugador
 {
-   
+   private String nombre;
+   private Mazo cartas;
+   private int puntuacion;
+
+   public Jugador(String nombre){
+      this.nombre = nombre;
+      puntuacion = 0;
+   }
 }
